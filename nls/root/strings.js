d46d79ea58adfb51a94ab71b5b214fd3dece48eb
Jeff Ward <jeffwardcoding@gmail.com>
2015-09-03 22:36:32 -0400

    added language selection and charset support

--- nls/root/strings.js
+++ nls/root/strings.js
@@ -24,15 +24,19 @@
 /*global define, brackets, $ */
 
 define({
-    'MENU_COMMAND': 'Generate HTML File...',
-    'TITLE_H1': 'HTML Settings',
-    'NO_FILE_OPEN_ERROR': 'There is no file open. Please open a file before running Generate-HTML.',
-    'TITLE_H4': '1.) Pick a DOCTYPE',
-    'CONTENT_ERROR': 'File has content, this action will overwrite that content.',
-    'SECTION_DOCTYPE': 'Standard DOCTYPES:',
-    'SELECT_LABEL': 'Choose...',
-    'SECTION_TEMPLATES': '2.) Pick the libraries you wish to use',
-    'SECTION_TEMPLATES_DESC': 'These libraries are hosted on Google CDN',
-    'GENERATE_BUTTON': 'Generate HTML',
-    'CANCEL_BUTTON': 'Cancel'
+  'MENU_COMMAND': 'Generate HTML File...',
+  'TITLE_H1': 'HTML Settings',
+  'LANGUAGE_SELECTION' : '1.) Select your language tag',
+  'LANGUAGE_DESCRIPTION': 'Supported Languages:',
+  'CHARSET_SELECTION': '2.) Select your charset tag',
+  'CHARSET_DESCRIPTION':'Available charset\'s:',
+  'NO_FILE_OPEN_ERROR': 'There is no file open. Please open a file before running Generate-HTML.',
+  'TITLE_H4': '3.) Select a DOCTYPE',
+  'CONTENT_ERROR': 'File has content, this action will overwrite that content.',
+  'SECTION_DOCTYPE': 'Standard DOCTYPES:',
+  'SELECT_LABEL': 'Choose...',
+  'SECTION_TEMPLATES': '4.) Select the libraries you wish to use',
+  'SECTION_TEMPLATES_DESC': 'These libraries are hosted on Google CDN',
+  'GENERATE_BUTTON': 'Generate HTML',
+  'CANCEL_BUTTON': 'Cancel'
 });
