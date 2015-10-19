db49fb7f4e01a543c4a91b7621bc57abef1669f7
Jeff Ward <jeffwardcoding@gmail.com>
2015-10-18 17:17:44 -0700

    fixed runtime errors

--- main.js
+++ main.js
@@ -19,163 +19,170 @@
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
  * DEALINGS IN THE SOFTWARE.
  */
-
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50, browser: true */
 /*global define, require, brackets, $, Mustache, chosenTemplate */
-
 require.config({
-  paths: {
-    "text": "lib/text",
-    "i18n": "lib/i18n"
-  },
-  locale: brackets.getLocale()
+    paths: {
+        "text": "lib/text",
+        "i18n": "lib/i18n"
+    },
+    locale: brackets.getLocale()
 });
 
-define(function (require, exports, module) {
+define(function(require, exports, module) {
 
-  'use strict';
+    'use strict';
 
-  var CommandManager = brackets.getModule('command/CommandManager'),
-      EditorManager = brackets.getModule('editor/EditorManager'),
-      Menus = brackets.getModule('command/Menus'),
-      Dialogs = brackets.getModule('widgets/Dialogs');
+    var CommandManager = brackets.getModule('command/CommandManager'),
+        EditorManager = brackets.getModule('editor/EditorManager'),
+        Menus = brackets.getModule('command/Menus'),
+        Dialogs = brackets.getModule('widgets/Dialogs');
 
-  // load up modal content, don't forget text! at beginning of file name
-  var modal = require('text!html/modal.html'),
-      Strings = require('strings');
+    // load up modal content, don't forget text! at beginning of file name
+    var modal = require('text!html/modal.html'),
+        Strings = require('strings');
 
-  //Define Libraries Scripts
+    //Define Libraries Scripts
 
-  //  var lib_angular = document.getElementById('angularjs').value;
-  //  var lib_angularMaterial = document.getElementById('angularmaterial').value;
-  //  var lib_dojo = document.getElementById('dojo').value;
-  //  var lib_extcore = document.getElementById('extcore').value;
-  //  var lib_jquery111 = document.getElementById('jquery111').value;
-  //  var lib_jquery214 = document.getElementById('jquery214').value;
-  //  var lib_jquerymobile = document.getElementById('lib_jquerymobile').value;
+    //  var lib_angular = document.getElementById('angularjs').value;
+    //  var lib_angularMaterial = document.getElementById('angularmaterial').value;
+    //  var lib_dojo = document.getElementById('dojo').value;
+    //  var lib_extcore = document.getElementById('extcore').value;
+    //  var lib_jquery111 = document.getElementById('jquery111').value;
+    //  var lib_jquery214 = document.getElementById('jquery214').value;
+    //  var lib_jquerymobile = document.getElementById('lib_jquerymobile').value;
 
-  //-----------------------------
+    //-----------------------------
 
-  //add event listener here
-//  $('#generate_html_button').on('click', templateBuilder.build);
-//this breaks it
-//  document.getElementById("generate_html_button").addEventListener("click", function(){ alert("Hello World!"); });
+    //add event listener here
+    //  $('#generate_html_button').on('click', templateBuilder.build);
+    //this breaks it
+    //  document.getElementById("generate_html_button").addEventListener("click", function(){ alert("Hello World!"); });
 
 
-  function generateHtml() {
+    function generateHtml() {
 
-    // Template builder is a module that is solely responsible for building templates based on user input.
-    var templateBuilder = function () {
+        // Template builder is a module that is solely responsible for building templates based on user input.
+        var templateBuilder = function() {
 
-      // Private: Gets checked boxes from the UI
-      function getCheckedBoxes(checkboxName) {
-        var checkboxes = document.getElementsByName(checkboxName);
+            // Private: Gets checked boxes from the UI
+            function getCheckedBoxes(checkboxName) {
+                var checkboxes = document.getElementsByName(checkboxName);
 
-        var checkedBoxes = checkboxes.map(function (checkbox) {
-          return checkbox.checked;
-        });
+                var checkedBoxes = [];
 
-        return checkedBoxes.length > 0 ? checkedBoxes : null;
-      }
+                for(var i = 0; i < checkboxes.length; i++) {
+                    if($(checkboxes[i]).prop('checked')) {
+                        checkedBoxes.push(checkboxes[i]);
+                    }
+                }
 
-      // Private: Creates language section based on user input
-      function getLanguageTag() {
-        var htmlTop = "<!DOCTYPE html><html><head lang='";
+                return checkedBoxes.length > 0 ? checkedBoxes : null;
+            }
 
-        var languageChoice = document.getElementById("languages").value;
+            // Private: Creates language section based on user input
+            function getLanguageTag() {
+                var htmlTop = "<!DOCTYPE html><html><head lang='";
 
-        var languageMap = {
-          "english": "en",
-          "german": "de",
-          "spanish": "es",
-          "french": "fr",
-          "italian": "it",
-          "chinese": "zh-cn"
+                var languageChoice = document.getElementById("languages").value;
+
+                var languageMap = {
+                    "english": "en",
+                    "german": "de",
+                    "spanish": "es",
+                    "french": "fr",
+                    "italian": "it",
+                    "chinese": "zh-cn"
+                };
+
+                if (languageMap[languageChoice]) {
+                    return htmlTop + languageMap[languageChoice] + "'>";
+                } else {
+                    return '';
+                }
+            }
+
+            // Private: Creates charset section based on user input
+            function getCharset() {
+                var htmlCharset_Beginning = "<meta charset='";
+                var htmlCharset_End = "'>" + "<title> -Insert Title- </title>" + "<!-- Insert CSS links below -->" + "</head>" + "<body>";
+                var charsetChoice = document.getElementById("charset").value;
+                if (charsetChoice === "utf8") {
+                    charsetChoice = "UTF-8";
+                    return htmlCharset_Beginning + charsetChoice + htmlCharset_End;
+                } else {
+                    charsetChoice = "UTF-16";
+                    return htmlCharset_Beginning + charsetChoice + htmlCharset_End;
+                }
+            }
+
+            // Private: Creates doctype based on user input
+            function getDoctype() {
+                return document.getElementById("doctype").value;
+            }
+
+            // Private: Adds libraries based on user input
+            function getLibraries() {
+                var checkedBoxes = getCheckedBoxes("lib_checkboxes");
+
+                var bottomHTML = '';
+
+                checkedBoxes.forEach(function(item) {
+                    bottomHTML += $(item).data('script');
+                });
+
+                bottomHTML += "</body></html>";
+
+                return bottomHTML;
+            }
+
+            function buildTemplate() {
+                var language = getLanguageTag(),
+                    charset = getCharset(),
+                    libraries = getLibraries(),
+                    template = language + charset + libraries;
+                console.log(template);
+                EditorManager.getCurrentFullEditor()._codeMirror.setValue(template);
+
+                hideModal();
+            }
+
+            // Here we define what functions are available in the templateBuilder module
+            return {
+                build: buildTemplate
+            };
+        }(); //End Template Builder
+
+        function bindEvents() {
+            $('#generate_html_btn').on('click', templateBuilder.build);
         };
 
-        if (languageMap[languageChoice]) {
-          return htmlTop + languageMap[languageChoice] + "'>";
-        } else {
-          return '';
-        }
-      }
+        function validateMainUI() {
+            var editor = EditorManager.getCurrentFullEditor();
 
-      // Private: Creates charset section based on user input
-      function getCharset() {
-        var htmlCharset_Beginning = "<meta charset='";
-        var htmlCharset_End = "'>" + "<title> -Insert Title- </title>" + "<!-- Insert CSS links below -->" + "</head>" + "<body>";
-        var charsetChoice = document.getElementById("charset").value;
-        if (charsetChoice === "utf8") {
-          charsetChoice = "UTF-8";
-          return htmlCharset_Beginning + charsetChoice + htmlCharset_End;
-        } else {
-          charsetChoice = "UTF-16";
-          return htmlCharset_Beginning + charsetChoice + htmlCharset_End;
-        }
-      }
+            if (editor && editor._codeMirror.getValue().length) {
+                $('#templates_warning').show();
+            } else {
+                $('#templates_error').show();
+            }
+        };
 
-      // Private: Creates doctype based on user input
-      function getDoctype() {
-        return document.getElementById("doctype").value;
-      }
+        function showModal() {
+            Dialogs.showModalDialogUsingTemplate(Mustache.render(modal, Strings));
+        };
 
-      // Private: Adds libraries based on user input
-      function getLibraries() {
-        var checkedBoxes = getCheckedBoxes("lib_checkboxes");
-        checkedBoxes.forEach(function (item) {
-          var scripts = + $(item).data('script');
-          var bottomHTML = scripts + "</body>" + "</html>";
-          return bottomHTML;
-        });
-      }
+        function hideModal() {
+            $('#templates_modalBtn').click();
+        };
 
-      function buildTemplate() {
-        var language = templateBuilder.getLanguage(),
-            charset = templateBuilder.getCharset(),
-            libraries = templateBuilder.getLibraries(),
-            template = language + charset + libraries;
-        console.log(template);
-        EditorManager.getCurrentFullEditor()._codeMirror.setValue(template);
+        showModal();
+        validateMainUI();
+        bindEvents();
+    } //End Generate HTML
 
-        hideModal();
-      }
-
-      // Here we define what functions are available in the templateBuilder module
-      return {
-        build: buildTemplate
-      };
-    } (); //End Template Builder
-
-    function bindEvents() {
-      $('#generate_html_btn').on('click', templateBuilder.build);
-    };
-
-    function validateMainUI() {
-      var editor = EditorManager.getCurrentFullEditor();
-
-      if (editor && editor._codeMirror.getValue().length) {
-        $('#templates_warning').show();
-      } else {
-        $('#templates_error').show();
-      }
-    };
-
-    function showModal() {
-      Dialogs.showModalDialogUsingTemplate(Mustache.render(modal, Strings));
-    };
-
-    function hideModal() {
-      $('#templates_modalBtn').click();
-    };
-
-    showModal();
-    validateMainUI();
-    bindEvents();
-  } //End Generate HTML
-
-  CommandManager.register(Strings.MENU_COMMAND, 'templates', generateHtml);
-  var menu = Menus.getMenu(Menus.AppMenuBar.EDIT_MENU);
-  menu.addMenuDivider();
-  menu.addMenuItem('templates');
+    CommandManager.register(Strings.MENU_COMMAND, 'templates', generateHtml);
+    var menu = Menus.getMenu(Menus.AppMenuBar.EDIT_MENU);
+    menu.addMenuDivider();
+    menu.addMenuItem('templates');
 
 }); //end define;
